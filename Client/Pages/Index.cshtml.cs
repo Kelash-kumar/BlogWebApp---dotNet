e8dc37270c32d7cd9538ae0dd3f69c53700661cb
Kelash Kumar <kelash.raisal@gmail.com>
2026-04-10 01:01:40 +0500

    fix issue

--- Client/Pages/Index.cshtml.cs
+++ Client/Pages/Index.cshtml.cs
@@ -1,13 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Client.Services;
+using Client.Models.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Client.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IBlogService _blogService;
 
+        public IndexModel(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public List<PostResponseDto> FeaturedPosts { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var result = await _blogService.GetPostsAsync(1, 3, sortBy: "createdAt", sortDirection: "desc");
+            if (result.Success && result.Data != null)
+            {
+                FeaturedPosts = result.Data.Data;
+            }
+            return Page();
         }
     }
 }
